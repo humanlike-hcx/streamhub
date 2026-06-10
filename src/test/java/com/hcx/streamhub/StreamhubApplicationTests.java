3c656866842a9a1415fe08ee7d3a95c89f1cbd0d
Probscray <hcx051005@gmail.com>
2026-06-10 23:35:16 +0800

    完成了登入，视频上传和解码，点赞收藏的功能

---
+++ src/test/java/com/hcx/streamhub/StreamhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcx.streamhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
